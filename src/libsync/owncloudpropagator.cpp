meskobalazs/desktop

implement single job to upload multiple files
Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

src/libsync/owncloudpropagator.cpp
@@ -817,7 +817,7 @@ Result<Vfs::ConvertToPlaceholderResult, QString> OwncloudPropagator::staticUpdat
 
 bool OwncloudPropagator::ignoreFilesUpload(const SyncFileItemPtr &item) const
 {
-    return !_scheduleDelayedJobs && !item->_isEncrypted;
+    return !_scheduleDelayedJobs && !item->_isEncrypted && _syncOptions._minChunkSize > item->_size;
 }
 
 void OwncloudPropagator::setScheduleDelayedJobs(bool active)

violethaze74-jazzededtech/desktop-1

src/libsync/owncloudpropagator.cpp
@@ -862,7 +862,7 @@ Result<Vfs::ConvertToPlaceholderResult, QString> OwncloudPropagator::staticUpdat
 
 bool OwncloudPropagator::isDelayedUploadItem(const SyncFileItemPtr &item) const
 {
-    return !_scheduleDelayedTasks && !item->_isEncrypted;
+    return !_scheduleDelayedTasks && !item->_isEncrypted && _syncOptions._minChunkSize > item->_size;
 }
 
 void OwncloudPropagator::setScheduleDelayedTasks(bool active)